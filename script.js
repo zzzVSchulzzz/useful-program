a077e82e46823b7c31689f9067673f43a18ca4b4
Valeri Medyanikova <valerijaschulz@gmail.com>
2024-11-13 19:39:03 +0300

    script.js aktualisieren

--- script.js
+++
@@ -1,8 +0,0 @@
-const buttonDown = document.querySelector('.btn.btn-danger')
-
-buttonDown.addEventListener('mousemove', (event) => {
-     const xPos = event.clientX;
-     const yPos = event.clientY;
-     buttonDown.style.left = `${xPos}px`;
-     buttonDown.style.top = `${yPos}px`;
-});
